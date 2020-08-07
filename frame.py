c71a7d0dc4c66f7d63bbfa2cda445b8aae50c61a
Jaime Andres <jaimeandres2998@gmail.com>
2020-08-06 19:44:41 -0500

    Corrections

--- frame.py
+++ frame.py
@@ -4,11 +4,11 @@ import random
 from rand import randomFile
 
 def crear_frame():
-    cap = cv2.VideoCapture("HaloCinematics/"+randomFile()) #video_name is the video being called
+    cap = cv2.VideoCapture("HaloCinematics/"+randomFile()) 
     total = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     n=random.randint(0,total)
     print("Frame "+str(n)+" out "+str(total))
-    cap.set(1,n) # Where frame_no is the frame you want
-    ret, frame = cap.read() # Read the frame
+    cap.set(1,n) 
+    ret, frame = cap.read() 
     cv2.imwrite("IMG/here.png",frame)
 
